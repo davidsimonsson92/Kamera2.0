c3102c1c5a0625d1b4c29cf5653704d04cc63117
David Simonsson <davidsimonsson_92@hotmail.com>
2025-03-29 10:50:35 +0100

    Initial Commit

---
+++ Kamera2.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kamera2.0
+//
+//  Created by David Simonsson on 2025-03-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kamera2.0/Kamera2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Kamera2_0App.swift
+//  Kamera2.0
+//
+//  Created by David Simonsson on 2025-03-29.
+//
+
+import SwiftUI
+
+@main
+struct Kamera2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
